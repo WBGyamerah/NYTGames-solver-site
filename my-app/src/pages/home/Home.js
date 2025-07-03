39fa3dcec5bfb99727809f83c0eb538c45aae7de
Stealthender <william.gyamerah@outlook.com>
2025-07-03 18:12:44 +0100

    Simple Home page created

---
+++ my-app/src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <header>
+                <h1>NYT Solver</h1>
+            </header>
+            <main>
+                <p> Hello </p>
+            </main>
+        </div>
+    );
+}
+
+export default HomePage;
